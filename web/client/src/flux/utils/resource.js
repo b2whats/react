eafcd6c853ea1f1f95c1cbed6df96dc77d6efa03
cybice <istarkov@gmail.com>
2014-12-27 01:21:22 +0300

    поддержка abort

--- web/client/src/flux/utils/resource.js
+++ web/client/src/flux/utils/resource.js
@@ -9,41 +9,41 @@ _.templateSettings = {
 var kCONTENT_TYPE_APPLICATION_JSON = {'Content-Type': 'application/json;charset=utf-8', 'Accept': 'application/json, text/plain, */*'};
 
 function http(method, url, object) {
-  return new q(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open(method, url, true);
-    
-    //если запрос cors то проставить хедер который не вызывает preflight запроса
-    if(url.indexOf('http://') === 0 || url.indexOf('https://') === 0 || url.indexOf('//') === 0) {
-      //не вызывает preflight запроса
-      xhr.setRequestHeader("Content-Type","text/plain");
-    }
-    
-    xhr.onreadystatechange = function () {
+  
+  var xhr = new XMLHttpRequest();        
+  xhr.open(method, url, true);
+  //если запрос cors то проставить хедер который не вызывает preflight запроса
+  if(url.indexOf('http://') === 0 || url.indexOf('https://') === 0 || url.indexOf('//') === 0) {
+    //не вызывает preflight запроса
+    xhr.setRequestHeader("Content-Type","text/plain");
+  }
 
-      if (this.readyState === this.DONE) {
-        if (this.status === 200) {
-          resolve(typeof(this.response) === 'string' ? JSON.parse(this.response) : this.response);
+  return {
+    promise: new q(function(resolve, reject) {                
+        xhr.onreadystatechange = function () {
+          if (this.readyState === this.DONE) {
+            if (this.status === 200) {
+              resolve(typeof(this.response) === 'string' ? JSON.parse(this.response) : this.response);
+            } else {
+              reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+            }
+          }
+        };
+
+        xhr.responseType = 'json';
+        _.each(kCONTENT_TYPE_APPLICATION_JSON, function(v, k) {
+          xhr.setRequestHeader(k, v);
+        });
+        
+        if(object!==null && object!==undefined) {
+          xhr.send(JSON.stringify(object));
         } else {
-          reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+          xhr.send();
         }
-      }
-    };
-
-    xhr.responseType = 'json';
-    _.each(kCONTENT_TYPE_APPLICATION_JSON, function(v, k) {
-      xhr.setRequestHeader(k, v);
-    });
-    
-    if(object!==null && object!==undefined) {
-      xhr.send(JSON.stringify(object));
-    } else {
-      xhr.send();
-    }
-  });
+      }),
+    abort: xhr.abort
+  };
 }
-
 
 var encode_object_properties = (obj) => {
   return _.reduce(obj, (memo, v, k) => {
@@ -58,12 +58,23 @@ module.exports = function(route) {
 
   return {
     get: function(obj) {
+      return http('GET', route_tpl(encode_object_properties(obj))).promise;
+    },    
+    get_ext: function(obj) {
       return http('GET', route_tpl(encode_object_properties(obj)));
     },
+
     post: function(obj) {
+      return http('POST', route_tpl(encode_object_properties(obj)), obj).promise;
+    },    
+    post_ext: function(obj) {
       return http('POST', route_tpl(encode_object_properties(obj)), obj);
     },
+    
     save: function(context, obj) {
+      return http('POST', route_tpl(encode_object_properties(context)), obj).promise;
+    },
+    save_ext: function(context, obj) {
       return http('POST', route_tpl(encode_object_properties(context)), obj);
     }
   };
