a3aac36d4c8086f0e04d2c4c57be4ceb080e64d1
cybice <istarkov@gmail.com>
2014-12-29 01:32:38 +0300

    fix потенциальный косяк который если бы случился не отловить, включение одного и тогоже миксина в два разных файла

--- web/client/src/flux/components/mixins/raf_state_update.js
+++ web/client/src/flux/components/mixins/raf_state_update.js
@@ -11,9 +11,9 @@ var event_names = require('shared_constants/event_names.js');
 var rafBatchStateUpdateMixinCreate = function(get_state) {
   var stores = [].slice.call(arguments).slice(1);
 
-  return {
-    event_disablers:[],
-  
+  return {    
+    __internal__counter__: {value:0}, //защита от подключения одного и того же миксина в два разных реакт класса - невозможная ситуация когда я код пишу
+
     getInitialState () {
       return get_state();
     },  
@@ -22,10 +22,12 @@ var rafBatchStateUpdateMixinCreate = function(get_state) {
       var state = get_state();
       raf(() => {
         this.replaceState(state)
-      }, null, this.constructor.displayName);
+      }, null, this.__internal__display_name__);
     },
     
-    componentWillMount () {
+    componentWillMount () {      
+      this.__internal__display_name__ = this.constructor.displayName + '__' + this.__internal__counter__.value++;
+
       this.event_disablers = _.map(stores, function(store) {
         return store.on(event_names.kON_CHANGE, this.on_change_handler);
       }, this);
