b0cbaf0ef2e387e439d90558eabff6785c4cffe5
cybice <istarkov@gmail.com>
2015-01-06 21:04:36 +0300

    ссылки на сервисы

--- web/client/src/flux/shared_constants/api_refs.js
+++ web/client/src/flux/shared_constants/api_refs.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  kAUTO_PART_SUGGESTER_API: 'http://autogiper.com/api/search.php?autoparts=:words',
-  kAUTO_SERVICE_SUGGESTER_API: 'http://autogiper.com/api/search.php?autoservices=:words',
+  kAUTO_PART_SUGGESTER_API: 'http://autogiper.com/api/search.php?type=autoparts&text=:words',
+  kAUTO_SERVICE_SUGGESTER_API: 'http://autogiper.com/api/search.php?type=autoservices&text=:words',
   kREGIONS_QUERY_API: 'http://autogiper.com/api/get/regions.php'
 };
