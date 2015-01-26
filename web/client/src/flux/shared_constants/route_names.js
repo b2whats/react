747946cb007fbec129bae9d1cc7f314fefcb1b1e
cybice <istarkov@gmail.com>
2015-01-27 02:04:53 +0300

    лишнее стер

--- web/client/src/flux/shared_constants/route_names.js
+++ web/client/src/flux/shared_constants/route_names.js
@@ -3,12 +3,10 @@
 var route_names = {
   kROUTE_DEF: '/',
   kROUTE_DEF_W_REGION: '/:region_id',
-  //kDEFAULT_ROUTE: 'kDEFAULT_ROUTE',
   
   kROUTE_CATALOG: '/catalog/:region_id',
   kROUTE_PARTS_FIND: '/find/:region_id/:sentence/:producer/:articul/:id/:service/:service_auto_mark/:service_id',
-  //kFIND_ROUTE: 'kFIND_ROUTE',
-
+  
   kHELP_ROUTE: 'kHELP_ROUTE'
 };
 
