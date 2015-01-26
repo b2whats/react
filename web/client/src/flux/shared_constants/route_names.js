cfd6a2cbf08dbfff05b3384dad5940254ee8882d
cybice <istarkov@gmail.com>
2015-01-27 02:03:57 +0300

    откатить на нормальную версию

--- web/client/src/flux/shared_constants/route_names.js
+++ web/client/src/flux/shared_constants/route_names.js
@@ -3,14 +3,11 @@
 var route_names = {
   kROUTE_DEF: '/',
   kROUTE_DEF_W_REGION: '/:region_id',
-  kDEFAULT_ROUTE: 'kDEFAULT_ROUTE',
+  //kDEFAULT_ROUTE: 'kDEFAULT_ROUTE',
   
   kROUTE_CATALOG: '/catalog/:region_id',
   kROUTE_PARTS_FIND: '/find/:region_id/:sentence/:producer/:articul/:id/:service/:service_auto_mark/:service_id',
-  kFIND_ROUTE: 'kFIND_ROUTE',
-
-  kROUTE_ACCOUNT_URI: '/account/company',
-  kROUTE_ACCOUNT: 'kROUTE_ACCOUNT',
+  //kFIND_ROUTE: 'kFIND_ROUTE',
 
   kHELP_ROUTE: 'kHELP_ROUTE'
 };
