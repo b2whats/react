efe84ceb8f6e25811241349ac54c4ee28db02c8b
cybice <istarkov@gmail.com>
2015-04-26 21:09:48 +0300

    добавил табличку, почему то тормозит

--- web2/flux/components/catalog_page_new/catalog_page_table_new.jsx
+++ web2/flux/components/catalog_page_new/catalog_page_table_new.jsx
@@ -7,18 +7,56 @@ var PureRenderMixin = React.addons.PureRenderMixin;
 var SizeHoc = require('components/hoc/size_hoc.js');
 
 var Link = require('components/link.jsx');
-var FixedDataTable = require('fixed-data-table');
-var Table = FixedDataTable.Table;
-var Column = FixedDataTable.Column;
+var {Table, Column} = require('fixed-data-table');
+require('fixed-data-table/dist/fixed-data-table.css');
+
+var getObjectAt = (i) => {
+
+  return {
+    name: `name ${i}`,
+    description: `description ${i}`,
+  };
+};
+
+var renderImage = (cell_data) => (
+  <div>cell_data</div>
+);
 
 var CatalogPageTableNew = React.createClass({
 
   mixins: [PureRenderMixin],
   
   render () {
+    
     return (
-      <div style={{backgroundColor: 'red'}} className="catalog-page-table-new">
-        jiopppppsss {this.props.width} {this.props.height}
+      <div style={{backgroundColor: 'red'}} className="catalog-page-table-new">        
+        {this.props.width && this.props.height ? 
+          <Table
+            width={this.props.width}
+            height={this.props.height}
+            rowHeight={40}
+            headerHeight={50}
+            rowGetter={getObjectAt}
+            rowsCount={100}
+            >
+
+          <Column
+            dataKey="name"
+            flexGrow={1}
+            label="xxx"
+            width={50} />
+        
+          <Column
+            dataKey="description"
+            
+            label="xxx"
+            flexGrow={1}
+            width={50} />
+
+          </Table>
+            :
+          null
+        }
       </div>
     );
   }
