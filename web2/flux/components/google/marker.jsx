b87e6f0f6d464812d84048b48a3c0d0a97e7603b
cybice <istarkov@gmail.com>
2015-05-05 03:49:03 +0300

    не самое убогое всплывающее окошко

--- web2/flux/components/google/marker.jsx
+++ web2/flux/components/google/marker.jsx
@@ -27,7 +27,12 @@ export default class CatalogPageRightBlockContentNew extends Component {
       <div className={cx('map-marker-holder map-marker-holder--as hint hint--top hint--info hint-html', (this.props.hover) ? 'hint--always hover' : 'hint--hidden')}>
         <div className="map-marker map-marker--as"></div>
         <div className="hint-content noevents map-marker-holder__small-hint">
-          {this.props.marker.get('title')}<strong>я хтмл</strong> хоть <i>с картинками</i> хоть без
+          <div>
+            <strong>Название {this.props.marker.get('title')}</strong>
+          </div>
+          <div>
+           <a className="ap-link">кликни на маркер для информации</a>
+          </div>
         </div>
       </div>
     );
