e3fb689b118905d53ff56a61f4ed2a806c0cce8f
W <b2whats@aol.com>
2015-09-25 00:31:42 -0700

    sort autoservice company (may be bugs)

--- web2/flux/components/SearchPage/search_page_auto_part_table1.jsx
+++ web2/flux/components/SearchPage/search_page_auto_part_table1.jsx
@@ -49,7 +49,7 @@ import statisticsActions from 'actions/statisticsActions.js';
 import Link from 'components/link.jsx';
 import regionStore from 'stores/region_store.js';
 //var search_page_actions = require('actions/search_page_actions.js');
-var kITEMS_PER_PAGE = [10, 20];
+var kITEMS_PER_PAGE = [5, 10, 20];
 var kPAGES_ON_SCREEN = sc.kPAGES_ON_SCREEN; //сколько циферок показывать прежде чем показать ...
 
 @controllable(['currentPage', 'itemPerPage', 'currentOrderItem'])
@@ -69,7 +69,7 @@ export default class SearchPageAutoPartTable extends Component {
 
   static defaultProps = {
     currentPage: 1,
-    itemPerPage: 10,
+    itemPerPage: 5,
     currentOrderItem: null
   }
   static propTypes = {
